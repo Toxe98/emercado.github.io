55357d53c60302e5d4b5497c2bb42462139fef62
Toxe98 <111653899+Toxe98@users.noreply.github.com>
2023-08-06 15:51:12 -0300

    adaptando la pagina

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
 function auth() {
-    let email = document.getElementById('floatinInput').value;
+    let email = document.getElementById('floatingInput').value;
     let password = document.getElementById('floatingPassword').value;
     if(email == 'admin@gmail.com' && password == '1234'){
         window.location.assign('index.html');
-        alert('Iniciando Sesion');
+        alert('Iniciando Sesión');     
     } else {
         alert("Datos Incorrectos, ingrese nuevamente");
         return;
-    }
+    }  
 };

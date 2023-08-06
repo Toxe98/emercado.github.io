d2c009a4d0be42bb2887238358c6c4b025686d4a
Toxe98 <111653899+Toxe98@users.noreply.github.com>
2023-08-05 23:36:27 -0300

    interacciones al iniciar sesion

--- js/login.js
+++ js/login.js
@@ -1,21 +1,11 @@
-// Constantes para el formulario y el boton de inciar sesion.
-const loginForm = document.getElementById("loginForm");
-const loginBtn = document.getElementById("loginBtn");
-
-
-// Agrego un evento al clickear el boton. Obtengo valores del input con datos del correo y la contraseña
-loginBtn.addEventListener('click', function() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    if (username.trim() === '' || password === '') {
-        
-        alert('Por favor, ingresa un usuario y contraseña válidos.');
-        
+function auth() {
+    let email = document.getElementById('floatinInput').value;
+    let password = document.getElementById('floatingPassword').value;
+    if(email == 'admin@gmail.com' && password == '1234'){
+        window.location.assign('index.html');
+        alert('Iniciando Sesion');
     } else {
-        window.location.href = 'index.html';
+        alert("Datos Incorrectos, ingrese nuevamente");
+        return;
     }
-});
-
-
-
+};
